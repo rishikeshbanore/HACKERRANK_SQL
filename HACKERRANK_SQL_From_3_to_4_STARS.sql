9e96cf8859313c6f85edcb3b7568376532f11793
Rishikesh BANORE <R.Banore@iaea.org>
2022-04-07 17:59:40 +0200

    Added Solution for PADS Problem.

--- HACKERRANK_SQL_From_3_to_4_STARS.sql
+++ HACKERRANK_SQL_From_3_to_4_STARS.sql
@@ -16,12 +16,24 @@
 		FROM TRIANGLES;
 
 
-/* PROBLEM STATEMENT: Type of Triangle
+/* PROBLEM STATEMENT:The PADS
 
-  Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+Generate the following two result sets:
 
-  Equilateral: It's a triangle with  sides of equal length.
-  Isosceles: It's a triangle with  sides of equal length.
-  Scalene: It's a triangle with  sides of differing lengths.
-  Not A Triangle: The given values of A, B, and C don't form a triangle
+Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
+
+There are a total of [occupation_count] [occupation]s.
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
+
+Note: There will be at least two entries in the table for each type of occupation.
 */
+
+
+select concat(Name,'(',substring(Occupation,1,1),')') as NAME 
+	from Occupations 
+	order by NAME;
+Select concat ('There are a total of ', count(Occupation),' ', lower(Occupation),'s.') as TOTALS
+	from Occupations
+	group by Occupation
+	order by TOTALS;
